7fac742692e9f560c0bb9f77d5816ca75a13214f
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-20 18:13:23 -0500

    Form to create new pokemon displaying on browser

--- views/New.jsx
+++ views/New.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function New() {
   return (
     <div>
-        New page to create new pokemon
+        <h1>Page to create a new pokemon</h1>
+        {/* NOTE: action will be the route, method will be the HTTP Method. NB: HTTP verb is Create, while HTTP method is POST */}
+        <form action="/pokemon/new" method='POST'>
+            Name: <input type="text" name='name' /> <br />
+            Image: <input type="text" name='image' />
+            {/* Image: <img src={pokemon.img} alt="" /> */}
+            <input type="submit" name='' value="Create New Pokemon" />
+
+        </form>
+       
     </div>
   )
 }
