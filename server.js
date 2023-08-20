058449c6896a8d9b08e1d695c00544db8f843013
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-20 06:37:20 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require("express");
 const app = express();
 const pokemon = require("./models/pokemon");  //NOTE: it must start with ./ if it's just a file, not an NPM package
 
+// Configure the app (app.set) .i.e  set up our view engine and this must be done above our routes
+app.set("view engine", "jsx");
+
+app.engine("jsx", require("express-react-views").createEngine());
+
+
 //First or Index Route (app.get)
 app.get('/', (req,res) => {
   res.send('Welcome to the Pokemon App!')
 })
 
 
-// Route to display pokemon data as json in the browser
+// Route (/pokemon) to display pokemon data as json in the browser (app.get)
 
-app.get('/pokemon', (req,res) => {
-   res.send(pokemon)
-})
+// app.get('/pokemon', (req,res) => {
+//   res.send(pokemon)
+// })
+
+app.get('/pokemon',(req,res) => {
+  res.render('Index')
+  //  res.render("Index", { header:'See All The Pokemon' })
+});
 
 
 
---
+++ views/Index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Index() {
+    // const myStyle = {
+    //     color: '#ffffff',
+    //     backgroundColor: '#000000',
+    //     };
+        
+        // class MyFirstComponent extends React.Component() {
+        // return (
+        //   <div  style={myStyle}>
+        //   <h3>My First React Component!</h3>
+        //   </div >
+        // )
+        // };
+    
+  return (
+    <div>
+        <h1>See All The Pokemon!!</h1>
+    </div>
+  )
+}
+
+export default Index
